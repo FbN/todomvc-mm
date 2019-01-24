37687b86e954672a7d16e21213d7f0b75463fdd3
Fabiano Taioli <ftaioli@gmail.com>
2019-01-24 15:44:50 +0100

    persist

--- js/model/todo.mjs
+++ js/model/todo.mjs
@@ -1,17 +1,21 @@
-/* global m, window */
+/* global m, localstorage */
 
 const STORAGE_ID = 'todos-mvvm-frp-mithril'
 
-const generateUniqueID = () => (new Date()).getTime()
+const generateUniqueID = () => new Date().getTime()
 
-export const list = JSON.parse(window.localStorage.getItem(STORAGE_ID) || '[]')
+export const list = m.stream(
+    JSON.parse(localStorage.getItem(STORAGE_ID) || '[]')
+)
+
+list.map(list => localStorage.setItem(STORAGE_ID, JSON.stringify(list)))
 
 export const add = title => {
-    list.push(
-        {
+    list(
+        list().concat({
             id: generateUniqueID(),
             title: title.trim(),
             completed: false
-        }
+        })
     )
 }
