b376722acddf66e6116ce12fac92f0d90667828d
Fabiano Taioli <ftaioli@gmail.com>
2019-04-21 16:25:53 +0200

    nice

--- app/js/vm/item.mjs
+++ app/js/vm/item.mjs
@@ -1,37 +1,52 @@
 import { m, M } from '../vendor.mjs'
 import { list, get, del, update } from '../model/todo.mjs'
 
+const makeAdapters = nameList =>
+    nameList.reduce(
+        (streamsAndTriggers, name) => {
+            ;[
+                streamsAndTriggers.triggers['_$' + name],
+                streamsAndTriggers.streams['$' + name]
+            ] = M.createAdapter()
+            return streamsAndTriggers
+        },
+        { streams: {}, triggers: {} }
+    )
+
 export default function itemVM (vnodeR) {
-    const [_$oninit, $oninit] = M.createAdapter()
-    const [_$keyup, $keyup] = M.createAdapter()
-    const [_$input, $input] = M.createAdapter()
-    const [_$complete, $complete] = M.createAdapter()
-    const [_$editing, $editing] = M.createAdapter()
-    const [_$delete, $delete] = M.createAdapter()
-    const [_$confirmEditing, $confirmEditing] = M.createAdapter()
-    const [_$listChanged, $listChanged] = M.createAdapter()
+    const { streams: S, triggers: T } = makeAdapters([
+        'oninit',
+        'onupdate',
+        'keyup',
+        'input',
+        'complete',
+        'editing',
+        'delete',
+        'confirmEditing',
+        'listChanged'
+    ])
 
-    const $enter = M.filter(e => e.keyCode === 13, $keyup)
-    const $esc = M.filter(e => e.keyCode === 27, $keyup)
+    const $enter = M.filter(e => e.keyCode === 13, S.$keyup)
+    const $esc = M.filter(e => e.keyCode === 27, S.$keyup)
 
-    const $confirmOnEnter = M.merge($confirmEditing, $enter)
+    const $confirmOnEnter = M.merge(S.$confirmEditing, $enter)
 
     const $item = M.map(
         () => (vnodeR.attrs.key && get(vnodeR.attrs.key)) || {},
-        M.startWith({}, $listChanged)
+        M.startWith({}, S.$listChanged)
     )
 
-    const $observeList = M.tap(() => list.map(_$listChanged), $oninit)
+    const $observeList = M.tap(() => list.map(T._$listChanged), S.$oninit)
 
     const $completeEffect = M.tap(
         update,
         M.map(item => {
             return Object.assign({}, item, { completed: !item.completed })
-        }, M.sample($item, $complete))
+        }, M.sample($item, S.$complete))
     )
 
     const $editingText = M.mergeArray([
-        M.map(e => e.target.value, $input),
+        M.map(e => e.target.value, S.$input),
         M.map(item => item.title, $item),
         M.map(item => item.title, M.sample($item, $esc))
     ])
@@ -51,32 +66,25 @@ export default function itemVM (vnodeR) {
     const $editingStatus = M.scan(
         v => !v,
         false,
-        M.mergeArray([$editing, $confirmEditingItem, $esc])
+        M.mergeArray([S.$editing, $confirmEditingItem, $esc])
     )
 
     const $itemRes = M.merge($item, $completeEffect, $confirmEditingItem)
 
-    const $deleteItem = M.tap(item => del(item.id), M.sample($item, $delete))
+    const $deleteItem = M.tap(item => del(item.id), M.sample($item, S.$delete))
 
-    // const $logUpdate = M.tap(e => console.log('update', e), $onupdate)
+    const $logUpdate = M.tap(e => console.log('update', e), S.$onupdate)
 
     return [
-        {
-            _$keyup,
-            _$input,
-            _$complete,
-            _$editing,
-            _$delete,
-            _$confirmEditing,
-            _$oninit
-        },
+        T,
         {
             $item: $itemRes,
             $editing: $editingStatus,
             $editingText,
             $observeList,
             $deleteItem,
-            $oninit
+            $oninit: $observeList,
+            $onupdate: $logUpdate
         }
     ]
 }
